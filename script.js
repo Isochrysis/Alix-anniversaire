61456ef67de80e283d66127b0b1eebe9eff8f106
Isochrysis <89359126+Isochrysis@users.noreply.github.com>
2021-12-17 14:37:02 -0500

    main

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let patoch =false;
 const patocheMusique = new Audio('musique/Joyeux Anniversaire - Patrick Sébastien.mp3');
-const drum = new Audio('musique/Bass Drum - Sound Effect.mp3');
+const drum = new Audio('musique/yeah.mp3');
 const jouerBatterie = () =>{
     
     if(!patoch){
